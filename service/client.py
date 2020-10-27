7a01713dbf84d1ebd50d8f6ce69eda2e650a4bcc
elbalucia <70612357+elbalucia@users.noreply.github.com>
2020-10-27 08:20:10 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -1,5 +1,4 @@
-""" Class representing a client
-    """
+""" Class representing a client """
 class Client():
 
     def __init__(self, name, last_name, doc_id):
@@ -7,16 +6,18 @@ class Client():
         self.last_name = last_name
         self.doc_id = doc_id
         self.preexistence = []
-
+        
+    """ Adds a preexistence to client """
     def add_preexistence(self, n_preexistence):
         self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
 
+    """ Adds a preexistence to client """
     def get_preexistence(self, p_index):
         if p_index >= len(self.preexistence):
             return 'There is no such preexistence'
-        else:
-            return self.preexistence[p_index]
+       
+        return self.preexistence[p_index]
 
     def get_all_preexistence(self):
         return self.preexistence
